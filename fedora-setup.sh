2b769e48b70ae5cfa16b17c55ce96528fe4c086a
Matt <16037839+mattyb74@users.noreply.github.com>
2020-03-20 10:47:41 +0000

    Update fedora-setup.sh

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 cd ~
 # Download files
-wget insert fedora packages here
+wget https://github.com/mattyb74/Fedora-Setup/blob/master/fedora.packages
 wget insert bash files here
 # Install Rpmfusion repo
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y

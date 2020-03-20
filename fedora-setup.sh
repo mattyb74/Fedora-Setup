99e980369432f110a0680f4d4b956184458fae8e
Matt <16037839+mattyb74@users.noreply.github.com>
2020-03-20 11:03:17 +0000

    Update fedora-setup.sh

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/bash
+
 cd ~
+
 # Download files
 wget https://github.com/mattyb74/Fedora-Setup/blob/master/fedora.packages
-# wget insert bash files here
-# Install Rpmfusion repo
+wget https://github.com/mattyb74/Fedora-Setup/blob/master/bashrc.aliases
+
+# Install Rpmfusion non & free repos
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
-# grab all packages to install
+# Grab and install packages from downloaded list
 sudo dnf install $(cat ~/fedora.packages) -y
 
-#cat ~/bashrc.aliases >> ~/.bashrc
+# Copy aliases from "bashrc.aliases to bashrc file
+cat ~/bashrc.aliases >> ~/.bashrc
 
-# Copy awesomevm files into correct location
+# Copy awesomewm files into correct location
 cp -R awesome /.config/awesome
 
 # Uninstall Gnome Desktop Environment
 sudo dnf remove gnome-desktop
-
-

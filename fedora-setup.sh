72df89242a30bc36abc36a440f1e24960594ee1d
Matt <16037839+mattyb74@users.noreply.github.com>
2020-03-20 12:51:48 +0000

    Update fedora-setup.sh

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -5,7 +5,9 @@ cd ~
 # Download files
 wget "https://github.com/mattyb74/Fedora-Setup/raw/master/fedora.packages"
 wget "https://github.com/mattyb74/Fedora-Setup/raw/master/bashrc.aliases"
-wget "https://github.com/mattyb74/Fedora-Setup/tree/master/awesome"
+wget "https://github.com/mattyb74/Fedora-Setup/blob/master/awesome/Wallpaper.png"
+wget "https://github.com/mattyb74/Fedora-Setup/blob/master/awesome/rc.lua"
+wget "https://github.com/mattyb74/Fedora-Setup/blob/master/awesome/theme.lua"
 
 # Install Rpmfusion non & free repos
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
